2e42b10be362a02477a29916b7c3832ad20a3dd5
Manoj Samanta <manoj.samanta@gmail.com>
2020-03-14 13:45:35 -0700

    added explanation on the code in examples folder

--- examples/khashl_test.c
+++ examples/khashl_test.c
@@ -5,6 +5,60 @@
 #include "khashl.h" // hash table
 KHASHL_MAP_INIT(, kc_c1_t, kc_c1, uint64_t, uint32_t, kh_hash_uint64, kh_eq_generic)
 
+//////////////////////////////////////////////////////
+//
+//	The parameters in the above macro -->
+//
+//	ignored first parameter = SCOPE for functions (e.g. static)
+//	kc_c1_t = name for data structures
+//	kc_c1 = prefix for functions
+//	uint64_t = khkey_t (hash key type)
+//	uint32_t = kh_val_t(hash value type)
+//	kh_hash_uint64 = see below
+//	kh_eq_generic = see below
+//
+//	kh_eq_generic = kh_eq_generic(a, b) ((a) == (b))
+//
+//	static kh_inline khint_t kh_hash_uint64(khint64_t key) {
+//		key = ~key + (key << 21);
+//		key = key ^ key >> 24;
+//        	key = (key + (key << 3)) + (key << 8);
+//        	key = key ^ key >> 14;
+//        	key = (key + (key << 2)) + (key << 4);
+//        	key = key ^ key >> 28;
+//        	key = key + (key << 31);
+//        	return (khint_t)key;
+//	}
+//
+//      The macro declaration gives you -
+//
+//      1. data structures -->
+//
+//        	typedef struct { \
+//                	khint_t bits, count; \
+//                	khint32_t *used; \
+//                	khkey_t *keys; \
+//        	} kc_c1_t;
+
+//
+//      2. functions -->
+//
+//      extern kc_c1_t *kc_c1_init(void); \
+//      extern void kc_c1_destroy(kc_c1_t *h); \
+//      extern void kc_c1_clear(kc_c1_t *h); \
+//      extern khint_t kc_c1_getp(const kc_c1_t *h, const khkey_t *key); \
+//      extern int kc_c1_resize(kc_c1_t *h, khint_t new_n_buckets); \
+//      extern khint_t kc_c1_putp(kc_c1_t *h, const khkey_t *key, int *absent); \
+//      extern void kc_c1_del(kc_c1_t *h, khint_t k);
+//
+//	kh_bucket(h, x)
+//	kh_size(h)
+//	kh_capacity(h)
+//	kh_end(h)
+//	kh_key(h, x)
+//	kh_val(h, x)
+//////////////////////////////////////////////////////
+
 int main(int argc, char *argv[])
 {
         int i, absent;
@@ -20,8 +74,6 @@ int main(int argc, char *argv[])
         if (absent) kh_val(h, itr) = 0;
         ++kh_val(h, itr);
 
-
-
         uint64_t cnt[256];
 
         for (i = 0; i < 256; ++i) cnt[i] = 0;
@@ -35,3 +87,4 @@ int main(int argc, char *argv[])
 	kc_c1_destroy(h);
 	return 0;
 }
+
--- examples/kseq_test.c
+++ examples/kseq_test.c
@@ -7,6 +7,40 @@
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
+//////////////////////////////////////////////////////
+//
+//	Parameters in the above macro -->
+//
+//	gzFile = type_t in the following data structure
+//	gzread = zlib function to read from file (default buffer size 16384)
+//	https://refspecs.linuxbase.org/LSB_3.0.0/LSB-Core-generic/LSB-Core-generic/zlib-gzread-1.html
+//
+//	The above macro declaration gives you -
+//
+//	1. data structures -->
+//
+//      typedef struct __kstream_t {
+//              unsigned char *buf;
+//              int begin, end, is_eof;
+//              type_t f;
+//      } kstream_t;
+//
+//      typedef struct { 
+//              kstring_t name, comment, seq, qual;
+//              int last_char;
+//              kstream_t *f;
+//      } kseq_t;
+//
+//	2. functions --> 
+//
+//      static kseq_t *kseq_init(gzFile fd)
+//	static int kseq_read(kseq_t *seq)
+//	static void kseq_destroy(kseq_t *ks)
+//
+//	The following code shows how they are used.
+//
+//////////////////////////////////////////////////////
+
 int main(int argc, char *argv[])
 {
 	gzFile fp;
